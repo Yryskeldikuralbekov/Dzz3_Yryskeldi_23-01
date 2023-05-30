af086deb5b5397adba91e39559f67e8da4bad709
Yryskeldikuralbekov <kuralyrys@gmail.com>
2023-05-30 21:27:23 +0600

    dz3

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 import { addTodo } from "./store/todosReducer";
 
 const TodoList = () => {
-  const [newtodo, setNewTodo] = useState([""]);
+  const [newtodo, setNewTodo] = useState("");
   const [newtodo1, setNewTodo1] = useState("");
   const [newtodo2, setNewTodo2] = useState("");
-  const user = useSelector((state) => state.user.name);
+  const user = useSelector((state) => state.name);
   const dispatch = useDispatch();
 
   const handleClick = () => {
@@ -25,27 +25,38 @@ const TodoList = () => {
   };
   return (
     <div>
-      TodoList
-      <input
-        placeholder="Введите имя"
-        type="text"
-        onChange={(e) => setNewTodo(e.target.value)}
-        value={newtodo}
-      />
-      <input
-        placeholder="Введите возраст"
-        type="number"
-        onChange={(e) => setNewTodo1(e.target.value)}
-        value={newtodo1}
-      />
-      <input
-        placeholder="Введите пол"
-        type="text"
-        onChange={(e) => setNewTodo2(e.target.value)}
-        value={newtodo2}
-      />
-      <button onClick={handleClick}>Добавить</button>
-      <ul>{user && user.map((u) => <li>{u}</li>)}</ul>
+      <div className="wrapper">
+        <div>
+          TodoList:
+          <input
+            className="input"
+            placeholder="Введите имя"
+            type="text"
+            onChange={(e) => setNewTodo(e.target.value)}
+            value={newtodo}
+          />
+          <input
+            className="input"
+            placeholder="Введите возраст"
+            type="number"
+            onChange={(e) => setNewTodo1(e.target.value)}
+            value={newtodo1}
+          />
+          <input
+            className="input"
+            placeholder="Введите пол"
+            type="text"
+            onChange={(e) => setNewTodo2(e.target.value)}
+            value={newtodo2}
+          />
+        </div>
+        <div>
+          <button className="btn" onClick={handleClick}>
+            Добавить
+          </button>
+        </div>
+      </div>
+      <ul>{user && user.map((user) => <li>{user}</li>)}</ul>
     </div>
   );
 };
--- src/store/todosReducer.js
+++ src/store/todosReducer.js
@@ -6,20 +6,18 @@ const initialState = {
   },
 };
 
-const todosReducer = (state = initialState, action) => {
+const todosReducer = (state = initialState.user, action) => {
   if (action.type == "ADD_TODO") {
-    return { ...state.user, user: [state.user.name, action.payload] };
+    return {
+      ...state,
+      name: [...state.name, action.payload],
+      age: [...state.age, action.payload],
+      gender: [...state.gender, action.payload],
+    };
   }
-  // if (action.type == "REMOVE_TODO") {
-  //   return state;
-  // }
-  // if (action.type == "SET_TODO") {
-  //   return { ...state, todos: action.payload };
-  // }
   return state;
 };
 
 export const addTodo = (payload) => ({ type: "ADD_TODO", payload });
-// export const deleteTodo = (payload) => ({ type: "ADD_TODO" });
 
 export default todosReducer;
